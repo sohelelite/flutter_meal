ef227451e9e7400ff9ad20f3325b20122db62bb5
Sohel <sohel.shaikh.s@gmail.com>
2021-04-20 01:32:52 +0530

    Theme implemented

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:meals/category_item.dart';
-import 'package:meals/dummy_data.dart';
+
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
@@ -10,12 +11,20 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
-        children: DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.title, catData.color)).toList(),
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                    catData.title,
+                    catData.color,
+                  ),
+            )
+            .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
-          mainAxisExtent: 20,
+          mainAxisSpacing: 20,
         ),
       ),
     );
