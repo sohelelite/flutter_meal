71fe64b81f0ee4f1c6f084e7a022745227198ecd
Sohel <sohel.shaikh.s@gmail.com>
2021-04-21 03:22:32 +0530

    Meal Item Screen implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './categories_screen.dart';
+import 'screens/category_meals_screen.dart';
+import 'screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,7 +28,11 @@ class MyApp extends StatelessWidget {
           ), 
         ),
       ),
-      home: CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        '/' : (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName : (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
